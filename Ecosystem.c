a347372e3eb8fc82e4b0af04ce37a020ebef3abf
duartepinto555 <duarte.pinto7337@gmail.com>
2023-12-02 12:32:40 +0100

    Added contiguous "normal" matrix

--- Ecosystem.c
+++ Ecosystem.c
@@ -10,16 +10,38 @@
 
 // Struct representing an Entity (either Rock/Rabbit/Empty/Fox)
 typedef struct {
-    int current_gen; // Current generation
+    int gen_proc; // Current generation
+    int gen_food; // Current generation
     char type; // <link>Entity</link> type: 'R' for rabbit, 'F' for fox
 } Entity;
 
-void printMatrix(Entity* matrix, int rows, int cols);
-void printAdjacentBlocks(Entity* matrix, int rows, int cols, int row, int col, char currentEntity, int* validMoves);
-void printPosition(int row, int col);
+void get_valid_moves(Entity**, int, int, int, int, int**);
+void next_generation(Entity**, int, int, int);
+int chooseMove(int*);
 
-int chooseMove(int* validMoves);
+void aloc_matrix(Entity*** matrix, int rows, int cols) {
+    Entity** tmp = (Entity**)malloc(rows * sizeof(Entity*));        // Allocate memory for the rows (Pointer to contiguous memory)
+    Entity* data = (Entity*)malloc(rows * cols * sizeof(Entity));   // Contiguous memory position which will actually store the data
+    for (int i=0; i<rows; i++) {
+        tmp[i] = &data[i*cols];     // Assign the starting address of each row in the contiguous memory block
+    }
 
+    *matrix = tmp;   // Set the pointer to the allocated matrix
+}
+
+void printPosition(int row, int col) {
+    printf("[%d, %d]\n", row, col);
+}
+void printMatrix(Entity** matrix, int rows, int cols) {
+    printf("Matrix array after allocating into a contiguous array:\n");
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            printf("%c ", matrix[i][j].type);
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
 void read_inputs(int* GEN_PROC_RABBITS, int* GEN_PROC_FOXES, int* GEN_FOOD_FOXES, int* N_GEN, int* R, int* C, int* N) {
     scanf("%d", GEN_PROC_RABBITS);
     scanf("%d", GEN_PROC_FOXES);
@@ -35,8 +57,9 @@ void load_ecosystem(Entity** matrix, int R, int C, int N) {
         for (int col=0; col<C; col++) {
             Entity new_entity;
             new_entity.type = EMPTY;
-            new_entity.current_gen = 0;
-            (*matrix)[row*R + col] = new_entity;
+            new_entity.gen_proc = 0;
+            new_entity.gen_food = -1;
+            matrix[row][col] = new_entity;
         }
     } 
     char obj[10]; // Assuming the maximum length of the object name is 10 characters
@@ -46,11 +69,12 @@ void load_ecosystem(Entity** matrix, int R, int C, int N) {
         
         // Map object representation to character
         if (strcmp(obj, "ROCK") == 0) {
-            (*matrix)[row*R + col].type = STONE;
+            matrix[row][col].type = STONE;
         } else if (strcmp(obj, "RABBIT") == 0) {
-            (*matrix)[row*R + col].type = RABBIT;
+            matrix[row][col].type = RABBIT;
         } else if (strcmp(obj, "FOX") == 0) {
-            (*matrix)[row*R + col].type = FOX;
+            matrix[row][col].type = FOX;
+            matrix[row][col].gen_food = 0;
         } else {
             printf("Unknown object type: %s\n", obj);
             exit(1); // Terminate program due to unknown object type
@@ -63,28 +87,31 @@ int main(int argc, char *argv[]) {
 
     read_inputs(&GEN_PROC_RABBITS, &GEN_PROC_FOXES, &GEN_FOOD_FOXES, &N_GEN, &rows, &cols, &N);
 
-    Entity* ecosystem = (Entity*)malloc(rows * cols * sizeof(Entity));
+    Entity** ecosystem ;
+    aloc_matrix(&ecosystem, rows, cols);
     // Load the ecosystem data into the matrix
-    load_ecosystem(&ecosystem, rows, cols, N);
+    load_ecosystem(ecosystem, rows, cols, N);
 
     // Print the matrix array after allocating it into a contiguous array
     printMatrix(ecosystem, rows, cols);
 
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            printf("Processing matrix[%d][%d] with entity '%c'\n", i, j, ecosystem[i * cols + j].type);
+    // obtain next gen
+    next_generation(ecosystem, rows, cols, 0);
+    // for (int i = 0; i < rows; ++i) {
+    //     for (int j = 0; j < cols; ++j) {
+    //         printf("Processing matrix[%d][%d] with entity '%c'\n", i, j, ecosystem[i][j].type);
 
-            int validMoves[4] = {-1, -1, -1, -1};
-            printAdjacentBlocks(ecosystem, rows, cols, i, j, ecosystem[i * cols + j].type, validMoves);
-            int chosenMove = chooseMove(validMoves);
-            if (chosenMove != -1) {
-                printf("Chosen move position: ");
-                printPosition(chosenMove / cols, chosenMove % cols);
-            } else {
-                printf("No valid move for matrix[%d][%d].\n", i, j);
-            }
-        }
-    }
+    //         int validMoves[4] = {-1, -1, -1, -1};
+    //         printAdjacentBlocks(ecosystem, rows, cols, i, j, ecosystem[i][j].type, validMoves);
+    //         int chosenMove = chooseMove(validMoves);
+    //         if (chosenMove != -1) {
+    //             printf("Chosen move position: ");
+    //             printPosition(chosenMove / cols, chosenMove % cols);
+    //         } else {
+    //             printf("No valid move for matrix[%d][%d].\n", i, j);
+    //         }
+    //     }
+    // }
 
     free(ecosystem);
 
@@ -92,64 +119,63 @@ int main(int argc, char *argv[]) {
 }
 
 
-void printMatrix(Entity* matrix, int rows, int cols) {
-    printf("Matrix array after allocating into a contiguous array:\n");
-    for (int i = 0; i < rows; ++i) {
-        for (int j = 0; j < cols; ++j) {
-            printf("%c ", matrix[i * rows + j].type);
+
+void next_generation(Entity** ecosystem, int rows, int cols, int G) {
+    // G -> Current Generation
+    for (int row=0; row<rows; row++) {
+        for (int col=0; col<cols; col++) {
+            int* valid_moves;
+            get_valid_moves(ecosystem, rows, cols, row, col, &valid_moves);
         }
-        printf("\n");
     }
-    printf("\n");
 }
-
-void printAdjacentBlocks(Entity* matrix, int rows, int cols, int row, int col, char currentEntity, int* validMoves) {
+void get_valid_moves(Entity** matrix, int rows, int cols, int row, int col, int** validMoves) {
     int count = 0;
+    char currentEntity = matrix[row][col].type; 
 
-    printf("Adjacent blocks for matrix[%d][%d] with entity '%c': ", row, col, currentEntity);
+    if (VERBOSE)
+        printf("Adjacent blocks for matrix[%d][%d] with entity '%c': ", row, col, currentEntity);
 
     // Right
-    if (col + 1 < cols && currentEntity == FOX && matrix[row * cols + col + 1].type == RABBIT) {
-        validMoves[count++] = (row * cols) + (col + 1);
-        printf("[%d, %d] ", row, col + 1);
-    } else if (col + 1 < cols && currentEntity == RABBIT && matrix[row * cols + col + 1].type == EMPTY) {
-        validMoves[count++] = (row * cols) + (col + 1);
-        printf("[%d, %d] ", row, col + 1);
+    if (col + 1 < cols && currentEntity == FOX && matrix[row][col + 1].type == RABBIT) {
+        *(validMoves)[count++] = (row * cols) + (col + 1);
+        if (VERBOSE) printf("[%d, %d] ", row, col + 1);
+    } else if (col + 1 < cols && currentEntity == RABBIT && matrix[row][col + 1].type == EMPTY) {
+        *(validMoves)[count++] = (row * cols) + (col + 1);
+        if (VERBOSE) printf("[%d, %d] ", row, col + 1);
     }
 
     // Left
-    if (col - 1 >= 0 && currentEntity == FOX && matrix[row * cols + col - 1].type == RABBIT) {
-        validMoves[count++] = (row * cols) + (col - 1);
-        printf("[%d, %d] ", row, col - 1);
-    } else if (col - 1 >= 0 && currentEntity == RABBIT && matrix[row * cols + col - 1].type == EMPTY) {
-        validMoves[count++] = (row * cols) + (col - 1);
-        printf("[%d, %d] ", row, col - 1);
+    if (col - 1 >= 0 && currentEntity == FOX && matrix[row][col - 1].type == RABBIT) {
+        *(validMoves)[count++] = (row * cols) + (col - 1);
+        if (VERBOSE) printf("[%d, %d] ", row, col - 1);
+    } else if (col - 1 >= 0 && currentEntity == RABBIT && matrix[row][col - 1].type == EMPTY) {
+        *(validMoves)[count++] = (row * cols) + (col - 1);
+        if (VERBOSE) printf("[%d, %d] ", row, col - 1);
     }
 
     // Up
-    if (row - 1 >= 0 && currentEntity == FOX && matrix[(row - 1) * cols + col].type == RABBIT) {
-        validMoves[count++] = ((row - 1) * cols) + col;
-        printf("[%d, %d] ", row - 1, col);
-    } else if (row - 1 >= 0 && currentEntity == RABBIT && matrix[(row - 1) * cols + col].type == EMPTY) {
-        validMoves[count++] = ((row - 1) * cols) + col;
-        printf("[%d, %d] ", row - 1, col);
+    if (row - 1 >= 0 && currentEntity == FOX && matrix[(row - 1)][col].type == RABBIT) {
+        *(validMoves)[count++] = ((row - 1) * cols) + col;
+        if (VERBOSE) printf("[%d, %d] ", row - 1, col);
+    } else if (row - 1 >= 0 && currentEntity == RABBIT && matrix[(row - 1)][col].type == EMPTY) {
+        *(validMoves)[count++] = ((row - 1) * cols) + col;
+        if (VERBOSE) printf("[%d, %d] ", row - 1, col);
     }
 
     // Down
-    if (row + 1 < rows && currentEntity == FOX && matrix[(row + 1) * cols + col].type == RABBIT) {
-        validMoves[count++] = ((row + 1) * cols) + col;
-        printf("[%d, %d] ", row + 1, col);
-    } else if (row + 1 < rows && currentEntity == RABBIT && matrix[(row + 1) * cols + col].type == EMPTY) {
-        validMoves[count++] = ((row + 1) * cols) + col;
-        printf("[%d, %d] ", row + 1, col);
+    if (row + 1 < rows && currentEntity == FOX && matrix[(row + 1)][col].type == RABBIT) {
+        *(validMoves)[count++] = ((row + 1) * cols) + col;
+        if (VERBOSE) printf("[%d, %d] ", row + 1, col);
+    } else if (row + 1 < rows && currentEntity == RABBIT && matrix[(row + 1)][col].type == EMPTY) {
+        *(validMoves)[count++] = ((row + 1) * cols) + col;
+        if (VERBOSE) printf("[%d, %d] ", row + 1, col);
     }
 
-    printf("\n");
+    if (VERBOSE) printf("\n");
 }
 
-void printPosition(int row, int col) {
-    printf("[%d, %d]\n", row, col);
-}
+
 
 int chooseMove(int* validMoves) {
     return validMoves[0];
